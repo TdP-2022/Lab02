00d01aff690f3764146a4b4b404bd3fae8b099ae
monge <alberto.monge@polito.it>
2022-03-10 19:13:13 +0100

    progetto base

---
+++ src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -0,0 +1,24 @@
+/**
+ * Sample Skeleton for 'Scene.fxml' Controller Class
+ */
+
+package it.polito.tdp.alien;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class FXMLController {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+
+    }
+
+}
